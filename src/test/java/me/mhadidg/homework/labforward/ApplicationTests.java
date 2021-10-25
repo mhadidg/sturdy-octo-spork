45a02b270d05333560e4d6330b8314ee9ae2c133
Mustapha Hadid <mustapha.hadeed@gmail.com>
2021-10-25 20:55:55 +0300

    Initial commit

---
+++ src/test/java/me/mhadidg/homework/labforward/ApplicationTests.java
@@ -0,0 +1,12 @@
+package me.mhadidg.homework.labforward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
